f3a7cb485ef92138a4fc239ef513498f3c028d6e
Aleksandr Khachich <95611562+Khanchic@users.noreply.github.com>
2022-10-05 17:23:28 +0300

    +

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,30 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+galleryItems.forEach((galleryItem) => {
+  galleryContainer.insertAdjacentHTML(
+    "beforeend",
+    `
+      <a class="gallery__item" href="${galleryItem.original}">
+        <img
+          class="gallery__image"
+          src=${galleryItem.preview}
+          data-source=${galleryItem.original}
+          alt=${galleryItem.description}
+        />
+      </a>
+    `
+  );
+});
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
